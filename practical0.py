49eff0a8cb4ec72b0890fc4d7b5bd79959e1c220
Tashey <tasheey604leo@gmail.com>
2025-02-26 22:38:05 +0600

    My second commit

--- practical0.py
+++ practical0.py
@@ -1,5 +1,4 @@
-studentName = "Sonam Choden"
-studentNumber = "02240122"
+studentName = "Jigme Zangmo"
+studentNumber = "02190102"
 
 print("My name is " + studentName + " and my student number is " + studentNumber)
-
